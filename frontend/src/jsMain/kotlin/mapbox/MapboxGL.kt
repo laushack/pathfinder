99cb3dbb954c4937ca626c2f8c19c0e2cfa92499
Alexandre Piveteau <alexandre@piveteau.email>
2023-12-03 02:04:02 +0100

    Add support for MapBox

---
+++ frontend/src/jsMain/kotlin/mapbox/MapboxGL.kt
@@ -0,0 +1,12 @@
+@file:JsNonModule
+@file:JsModule("mapbox-gl")
+
+package mapbox
+
+/** Mapbox access token. */
+external var accessToken: String
+
+/** A Mapbox map. */
+external class Map(options: MapOptions = definedExternally) {
+  fun remove()
+}
